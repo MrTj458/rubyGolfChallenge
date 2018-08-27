994ac5f857850ae0d06d9a669a48a2e6bd75505c
MrTj458 <mrtj458@gmail.com>
2018-08-27 14:26:10 -0600

    Added RPC

--- main.rb
+++ main.rb
@@ -6,3 +6,25 @@ def sumArray(a)
   a.each {|n| t += n}
   t
 end
+
+# Rock Paper Scissors
+def RPC(uc)
+  c = ['r', 'p', 's']
+  cc = c.sample
+  puts "#{uc} vs. #{cc}"
+
+  if uc == cc
+    return 't'
+  end
+
+  case uc
+  when 'r'
+    cc == 'p' ? r = 'l' : r = 'w'
+  when 'p'
+    cc == 's' ? r = 'l' : r = 'w'
+  when 's'
+    cc == 'r' ? r = 'l' : r = 'w'
+  end
+
+  return r
+end
